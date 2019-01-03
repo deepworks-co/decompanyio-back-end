45014a0cc8c67a630979c2fdab9da0c94503086e
jay <ymggt@naver.com>
2019-01-03 17:42:21 +0900

    개발용 PV API 제공하기 response format 수정 {yyyymmdd: [{id: vc}]} 예시 : {20190103: [{id: asfsafasfsa, vc:23}..... ] https://www.wrike.com/open.htm?id=303883446

--- controllers/oraclizeController/index.js
+++ controllers/oraclizeController/index.js
@@ -1,14 +1,22 @@
 'use strict';
 const uuidv4 = require('uuid/v4');
 module.exports.handler = (event, context, callback) => {
-  var result = [];
+  const resultList = [];
+  let yyyymmdd = getYYYYMMDD();
+  if(event.queryStringParameters && event.queryStringParameters.d){
+    yyyymmdd = event.queryStringParameters.d;
+  }
+
   for(var i=0;i<500;i++){
     let item = {};
     item.id = uuidv4().replace(/-/gi, "");
-    item.viewcount = getRndInteger(0, 1000);
-    result.push(item);
+    item.vc = getRndInteger(0, 1000);
+    resultList.push(item);
   }
 
+
+  const result  = {};
+  result[yyyymmdd] = resultList;
   const response = {
     statusCode: 200,
     body: JSON.stringify(result),
@@ -22,4 +30,15 @@ module.exports.handler = (event, context, callback) => {
 
 const getRndInteger = (min, max) => {
   return Math.floor(Math.random() * (max - min) ) + min;
+}
+
+const getYYYYMMDD = () => {
+  var x = new Date();
+  var y = x.getFullYear().toString();
+  var m = (x.getMonth() + 1).toString();
+  var d = x.getDate().toString();
+  (d.length == 1) && (d = '0' + d);
+  (m.length == 1) && (m = '0' + m);
+  var yyyymmdd = y + m + d;
+  return yyyymmdd;
 }
