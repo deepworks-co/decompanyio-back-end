c6921921d4cacc273dc2dc9faa79ee38e529b7f5
jay <ymggt@naver.com>
2019-01-25 23:34:34 +0900

    create batch-pageview-athena project

--- decompany-modules/decompany-common-utils/index.js
+++ decompany-modules/decompany-common-utils/index.js
@@ -1,6 +1,7 @@
 const utils = require('./utils');
 const s3Utils = require('./s3Utils');
+const mongoWapper = require('./MongoWapper');
 
 module.exports = utils;
-
 module.exports.s3 = s3Utils;
+module.exports.MongoWapper = mongoWapper;
--- decompany-modules/decompany-common-utils/utils.js
+++ decompany-modules/decompany-common-utils/utils.js
@@ -1,3 +1,6 @@
+/**
+ * @description Date object를 Blockchain에 넣을 YYYY-MM-DD 00:00:00의 timestamp값으로 변경한다. 
+ */
 exports.getBlockchainTimestamp = (date) => {
   // daily YYYY-MM-DD 00:00:00(실행기준에서 전날 일자)
   //let yesterday = new Date(); /* 현재 */
@@ -5,9 +8,27 @@ exports.getBlockchainTimestamp = (date) => {
 
   /* 날짜 - 1일 */
 
-  const d = Math.floor(date / (60 * 60 *24 * 1000)) * (60 * 60 *24 * 1000);
+  if(typeof(date) === 'string'){
+    //yyyy-mm-dd string
+    const yyyymmdd = toDate(date);
+    const d = Math.floor(yyyymmdd / (60 * 60 *24 * 1000)) * (60 * 60 *24 * 1000);
+    return d;
+  } else if(typeof(date) === 'object'){
+    //Date type
+    const d = Math.floor(date / (60 * 60 *24 * 1000)) * (60 * 60 *24 * 1000);
+    return d;
+  } else {
+    throw new Error('Unsupported datatype.' + typeof(date));
+  }
+
+  
   //console.log("getBlockchainTimestamp", d, new Date(d));
   return d;
+}
+
+function toDate(dateStr) {
+  const [year, month, day] = dateStr.split("-")
+  return new Date(year, month - 1, day)
 }
 
 exports.getNumber = (number, defaultNumber) => {
