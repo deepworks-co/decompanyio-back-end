9ce2d2ea1d571e9dd514d37a2744b272297bd25b
jay <ymggt@naver.com>
2019-03-06 16:31:43 +0900

    [A20181126_0023] Top 1000 tag 목록 열람하기   - bug fix : return 추가

--- batch-pageview/functions/cron/generateTopTag.js
+++ batch-pageview/functions/cron/generateTopTag.js
@@ -8,25 +8,13 @@ const TB_TOPTAG = tables.TOP_TAG;
 
 
 module.exports.handler = async (event, context, callback) => {
-
   
   const wapper = new MongoWapper(mongodb.endpoint);
   const mapReduceResult = await wapper.mapReduce(TB_DOCUMENT, map, reduce, {out: TB_TOPTAG}); //{out: {inline: 1}}
   const resultList = await wapper.find(TB_TOPTAG, {});
   console.log(resultList);
 
-  const response = {
-    statusCode: 200,
-    body: JSON.stringify({
-      message: 'Go Serverless v1.0! Your function executed successfully!',
-      input: event,
-    }),
-  };
-
-  callback(null, response);
-
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // callback(null, { message: 'Go Serverless v1.0! Your function executed successfully!', event });
+  return (null, "success");
 };
 
 
