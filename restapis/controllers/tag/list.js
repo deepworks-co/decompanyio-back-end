0f92a34c59566af5756727429657d874b8339ad5
jay <ymggt@naver.com>
2019-03-11 17:59:41 +0900

    cors 적용

--- restapis/controllers/tag/list.js
+++ restapis/controllers/tag/list.js
@@ -7,6 +7,10 @@ module.exports.handler = async (event, context, callback) => {
 
   const response = {
     statusCode: 200,
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Credentials': true,
+    },
     body: JSON.stringify({
       success: true,
       resultList: resultList
