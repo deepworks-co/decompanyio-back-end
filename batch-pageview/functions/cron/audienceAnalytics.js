c55e23bbb3241f0884d9e48e1f78b9903ab8f3b1
jay <ymggt@naver.com>
2019-03-13 11:55:26 +0900

    popular 순위를 집계 개선 및 Audience analytics 개발
    - Audience analytics - 페이지 별 및 문서 별 view count 집계 결과 열람하기 (batch)
    일자/시간별, 문서별,  view count 집계 및 조회 할수 있는 Table영역에 Write

---
+++ batch-pageview/functions/cron/audienceAnalytics.js
@@ -0,0 +1,76 @@
+'use strict';
+const {utils, MongoWapper} = require('decompany-common-utils');
+const { mongodb, tables } = require('../../resources/config.js').APP_PROPERTIES();
+
+
+const TB_TRACKING = tables.TRACKING;
+const TB_STAT_PAGEVIEW_DAILY = tables.STAT_PAGEVIEW_DAILY;
+module.exports.handler = async (event, context, callback) => {
+  //const startTimestamp = new Date(now - 1000 * 60 * 60 * 1); //1시간전
+  const startTimestamp = 0;
+
+  const wapper = new MongoWapper(mongodb.endpoint);
+
+  const queryPipeline = [{
+    $match: {
+      t: {$gte: startTimestamp}
+    }
+  }, {
+    $sort: {
+      t: 1
+    }
+  }, {
+    $group: {
+      _id: {
+        year: {$year: {$add: [new Date(0), "$t"]}}, 
+        month: {$month: {$add: [new Date(0), "$t"]}}, 
+        dayOfMonth: {$dayOfMonth: {$add: [new Date(0), "$t"]}},
+        id: "$id",
+        cid: "$cid",
+        sid: "$sid"
+      }
+    }
+  }, {
+    $group: {
+      _id: {
+        year: "$_id.year", 
+        month: "$_id.month", 
+        dayOfMonth: "$_id.dayOfMonth",
+        id: "$_id.id",
+      },
+      count: {$sum: 1},
+    }
+  }, {
+    $addFields: {
+      year:"$_id.year",
+      month:"$_id.month",
+      dayOfMonth:"$_id.dayOfMonth",
+      documentId:"$_id.id",
+    }
+  }, {
+    $sort:{year: 1, month:1, dayOfMonth:1}
+  }] 
+
+  
+  const resultList = await wapper.aggregate(TB_TRACKING, queryPipeline, {
+    allowDiskUse: true
+  });
+
+  const bulk = wapper.getUnorderedBulkOp(TB_STAT_PAGEVIEW_DAILY);
+  resultList.forEach((item)=>{
+    const statDate = new Date(Date.UTC(item.year, item.month-1, item.dayOfMonth));
+    item.created = Date.now();
+    item.statDate = statDate;
+    //console.log(item);
+    //bulk.insert(item);
+    bulk.find(item._id).upsert().updateOne(item);
+  });
+
+  console.log("total query result count", resultList.length);
+  console.log("bulk ops", bulk.tojson());
+  const result = await wapper.execute(bulk);
+  //console.log("10 list", resultList.slice(0, 73), "....");
+  console.log("bulk result", result);
+  wapper.close();
+  return (null, resultList);
+};
