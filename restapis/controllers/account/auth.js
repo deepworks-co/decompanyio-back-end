4b3a5976b3af864db6b39277a5079990995f2ba8
jay <ymggt@naver.com>
2019-03-19 11:25:23 +0900

    auth.js 오류 리턴 방식을 callback -> throw error 으로 변경

--- restapis/controllers/account/auth.js
+++ restapis/controllers/account/auth.js
@@ -10,7 +10,7 @@ module.exports.handler = async (event, context, callback) => {
   console.log("event", event);
   console.log("authorizationToken", event.authorizationToken);
   if (!event.authorizationToken) {
-    return callback('Unauthorized');
+    throw new Error('Unauthorized');
   }
 
   const tokenParts = event.authorizationToken.split(' ');
@@ -18,7 +18,7 @@ module.exports.handler = async (event, context, callback) => {
 
   if (!(tokenParts[0].toLowerCase() === 'bearer' && tokenValue)) {
     // no auth token!
-    return callback('Unauthorized');
+    throw new Error('Unauthorized');
   }
   const options = {
     audience: AUTH0_CLIENT_ID,
@@ -30,7 +30,7 @@ module.exports.handler = async (event, context, callback) => {
         console.log('verifyError', verifyError);
         // 401 Unauthorized
         console.log(`Token invalid. ${verifyError}`);
-        return callback('Unauthorized');
+        throw new Error('Unauthorized');
       }
       // is custom authorizer function
       console.log('valid from customAuthorizer', decoded);
@@ -49,6 +49,7 @@ module.exports.handler = async (event, context, callback) => {
 const generatePolicy = (principalId, effect, resource) => {
   const authResponse = {};
   authResponse.principalId = principalId;
+  authResponse.claims = {principalId}
   if (effect && resource) {
     const policyDocument = {};
     policyDocument.Version = '2012-10-17';
