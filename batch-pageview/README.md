f331e5d887d4f513867896252f9a9278d7a3fbe8
jay <ymggt@naver.com>
2019-03-22 10:21:34 +0900

    onchain batch 개선 - 유효한 curator 문서 처리, vesting 기간 30일, pageview 의 묶음 처리
     - vote event 수집 배치 적용

--- batch-pageview/README.md
+++ batch-pageview/README.md
@@ -31,6 +31,7 @@ sls create function -f voteRequestWriteOnchain --handler functions/cron/requestW
 sls create function -f readLatestVoteAmount --handler functions/onchain/readLatestVoteAmount.handler
 sls create function -f readLatestCreatorReward --handler functions/onchain/readLatestCreatorReward.handler
 sls create function -f hourlyReadVote --handler functions/cron/hourlyReadVote.handler
+sls create function -f voteCollector --handler functions/cron/voteCollector.handler
 
 # Test
 
--- batch-pageview/functions/ContractWapper.js
+++ batch-pageview/functions/ContractWapper.js
@@ -12,21 +12,40 @@ module.exports = class ContractWapper {
     const providerUrl = ethereum.providerUrl;
     const account = ethereum.account;
     const privateKey = ethereum.privateKey;
-    const networkIndex = ethereum.index;
-
+    
     this.web3 = new Web3(new Web3.providers.HttpProvider(providerUrl));
     this.contractABI = contract.abi;
+
     this.myAddress = account;
     this.privateKey = Buffer.from(privateKey, 'hex');
     //contract abi is the array that you can get from the ethereum wallet or etherscan
-    this.contractAddress = contract.networks[networkIndex].address;
+    this.contractAddress = contract.networks[ethereum.index].address;
     //creating contract object
     this.DocumentReg = new this.web3.eth.Contract(this.contractABI, this.contractAddress, {
       from: this.myAddress
     });
+        
     this.confirmPageViewContract = this.DocumentReg.methods.confirmPageView;
   }
  
+  updateSignature(logging){
+    return this.contractABI.map((item, index)=>{
+      switch(item.type){
+          case "event":
+              item.signature = this.web3.eth.abi.encodeEventSignature(item);
+              if(logging) console.log(`[${index}]`, item.type, item.name, item.signature)
+              
+              break;
+          case "function":
+              item.signature = this.web3.eth.abi.encodeEventSignature(item);
+              if(logging) console.log(`[${index}]`, item.type, item.name, item.signature);
+              
+              break;
+      }
+
+      return item;
+    })
+  }
 
   getPrepareTransaction() {
     return new Promise((resolve, reject) => {
@@ -98,6 +117,10 @@ module.exports = class ContractWapper {
     return this.web3.utils.asciiToHex(str);
   }
 
+  hexToAscii(hex){
+    return this.web3.utils.hexToAscii(hex);
+  }
+
 
   isExistsDocument(documentId) {
     return this.DocumentReg.methods.contains(this.asciiToHex(documentId)).call({from: this.myAddress})
@@ -136,6 +159,110 @@ module.exports = class ContractWapper {
     console.log(curatorId, documentId, viewCount, totalViewCount);
     return this.DocumentReg.methods.calculateCuratorReward(curatorId, this.asciiToHex(documentId), viewCount, totalViewCount).call({from: this.myAddress});
   }
+
+
+  async getEventLogs(latestCollectedBlockNumber, eventName) {
+
+    if(!eventName){
+      throw new Error(`event is not invalid!! ${eventName}`);
+    }
+    
+    const selectedAbi = this.contractABI.find((abi, index)=>{
+      return abi.name === eventName;
+    });
+
+    if(!selectedAbi){
+      throw new Error(`${eventName} is not exists in abi`)
+    }
+    const signature = selectedAbi.signature;//this.web3.eth.abi.encodeEventSignature(selectedAbi);
+
+    //console.log(eventName, signature, selectedAbi);
+
+    const pastLogs = await this.web3.eth.getPastLogs({
+      fromBlock: latestCollectedBlockNumber,
+      toBlock: "latest",
+      address: this.contractAddress,
+      topics: [ signature/*"0x0c75c6a0cc6b6403ea7acee71d8aa1556e09a34e8560c6f9ab5b6152bfcd8ef1"*/, null, null]
+    });
+    const promises = await pastLogs.map(async (log, index)=>{
+      const block = await this.getBlock(log.blockNumber);
+      //console.log("pastLogs", index, log);
+      //return await this.getVoteTransactionReceipt(transaction, block)
+      const decoded = await this.getDecodedLog(log, selectedAbi.inputs);
+
+      return {
+        abi: selectedAbi,
+        decoded: decoded,
+        created: block.timestamp * 1000,
+        log: log
+      }
+    });
+    const resultList = await Promise.all(promises);
+    console.log(resultList);
+    return resultList;
+  }
+
+  async getTransactionReceipt(transaction){
+
+    return new Promise(async (resolve, reject)=>{
+        
+      this.web3.eth.getTransactionReceipt(transaction.transactionHash,  async (err, receipt) => {
+        if(err) reject(err);
+        else resolve(receipt);
+      })
+    });
+  }
+
+  async getVoteEvents(receipt, block){
+    
+    //console.log(`receipt ${JSON.stringify(receipt)}, blockNumber : ${receipt.blockNumber}, timestamp : ${new Date(blockTimestamp)}`)    
+    const promises = await receipt.logs.filter((log)=> {
+
+      return this.contractSignatureAbi.find((abi)=>{
+        return abi.signature === log.topics[0]
+      });
+
+    }).map((log, idx)=>{
+      console.log("map", idx, log);
+      const selecedAbi = this.contractSignatureAbi.find((abi)=>{
+        return abi.signature === log.topics[0]
+      });
+
+      const decoded = this.web3.eth.abi.decodeLog(selecedAbi.inputs, log.data, log.topics.splice(1));
+      
+      //console.log("decoded", new Date(blockTimestamp), selecedAbi.name, decoded);
+      const event = {
+        abi: selecedAbi,
+        decoded: decoded,
+        created: block.timestamp * 1000,
+        receipt
+      }
+      return event;
+    });
+    const events = await Promise.all(promises);
+    
+    return events;
+  }
+
+  async getDecodedLog(log, inputs){
+  
+    const decoded = this.web3.eth.abi.decodeLog(inputs, log.data, log.topics.splice(1));
+    
+    return decoded;
+  }
+
+  async getBlock(blockNumber) {
+
+    return new Promise((resolve, reject) =>{
+        this.web3.eth.getBlock(blockNumber, function(err, block){
+            if(err) reject(err);
+            else resolve(block);
+        })
+    })
+      
+  }
+
+  
 }
 
 
