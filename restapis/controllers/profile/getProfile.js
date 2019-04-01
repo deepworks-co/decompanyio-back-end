349752a1f09c78acb48a0bad2938b238e0f7d159
jay <ymggt@naver.com>
2019-04-01 18:19:04 +0900

    /api/profile/get api 에서 username으로 조회하기

--- restapis/controllers/profile/getProfile.js
+++ restapis/controllers/profile/getProfile.js
@@ -4,16 +4,20 @@ const AccountService = require('../account/AccountService');
 module.exports.handler = async (event, context, callback) => {
   console.log(JSON.stringify(event));
   const {query} = event;
-
-  if(!query || !query.email){
-    throw new Error("parameter is invalid!!");
-  }
+  const {username, email} = query;
 
   const accountService = new AccountService();
 
-  const user = await accountService.getUserInfo({
-    email: query.email
-  }, {email: 1, name: 1, username:1, picture: 1, nickname: 1, family_name:1 });
+  const params = {}
+  if(email){
+    params.email = email;
+  } else if(username) {
+    params.username = username;
+  } else {
+    throw new Error("parameter is invalid!!")
+  }
+
+  const user = await accountService.getUserInfo(params, {email: 1, name: 1, username:1, picture: 1, nickname: 1, family_name:1 });
   
   if(!user){
     throw new Error("user is not exists... " + JSON.stringify(query));
