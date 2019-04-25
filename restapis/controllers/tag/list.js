341372aa74d2623177761b2c0cb1e2141a9e4217
jay <ymggt@naver.com>
2019-04-25 13:20:31 +0900

    tagList response max-age=120적용

--- restapis/controllers/tag/list.js
+++ restapis/controllers/tag/list.js
@@ -4,17 +4,10 @@ module.exports.handler = async (event, context, callback) => {
 
   const resultList = await documentService.getTopTag();
 
-  const response = {
-    statusCode: 200,
-    headers: {
-      'Access-Control-Allow-Origin': '*',
-      'Access-Control-Allow-Credentials': true,
-    },
-    body: JSON.stringify({
-      success: true,
-      resultList: resultList
-    }),
-  };
+  const response = JSON.stringify({
+    success: true,
+    resultList: resultList
+  });
 
-  return (null, response);
+  return response;
 };
