a5aec5ef1efcf110218b47e3b6f1d41b2faff3f2
jay <ymggt@naver.com>
2019-04-30 10:08:36 +0900

    [P1]API인증 로직 및 Zapier 연동
     - 2페이지 이상 읽은 유저만 조회

--- restapis/controllers/zapier/email.js
+++ restapis/controllers/zapier/email.js
@@ -7,7 +7,7 @@ module.exports.handler = async (event, context, callback) => {
   const {documentId} = event.query;
   
   const start = Date.now() - (1000 * 60 * 60 * 24 * 1);
-  const emails = await wapper.distinct(tables.TRACKING, "e", {id: documentId, e: {$regex: /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/}, t:{$gt: start}});
+  const emails = await wapper.distinct(tables.TRACKING, "e", {id: documentId, n: {$gt: 1}, e: {$regex: /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/}, t:{$gt: start}});
 
   const results = emails.map((e)=>{
     return {
@@ -17,7 +17,6 @@ module.exports.handler = async (event, context, callback) => {
   })
   
   console.log("results", results);
-  //const emails = ["jay@decompany.io", "jay.j.lee@infrawareglobal.com"];
 
   return JSON.stringify(results);
 };
