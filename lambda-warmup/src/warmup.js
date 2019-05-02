70b39daa6a8907dd0ecc61b2912f69bbf6ed8b58
jay <ymggt@naver.com>
2019-05-02 16:23:16 +0900

    update

--- lambda-warmup/src/warmup.js
+++ lambda-warmup/src/warmup.js
@@ -4,23 +4,26 @@ const aws = require("aws-sdk");
 const lambda = new aws.Lambda({region: region}); 
 
 module.exports.handler = async (event, context, callback) => {
+
   
-  const prefix = warmupConfig.prefix;
-  const functions = warmupConfig.functions;
+  const {prefix, functions} = warmupConfig;
+
   const payload = JSON.stringify({ source: 'lambda-warmup'});
   
   const result = await Promise.all(functions.map(async (func) => {
-
+    const functionName = prefix?prefix.concat(func.name):func.name;
     const params = {
       ClientContext: Buffer.from(payload).toString('base64'),
-      FunctionName: func.functionName,
+      FunctionName: functionName,
       InvocationType: "RequestResponse",
       LogType: "None",
       Qualifier: "$LATEST",
       Payload: payload
     };
-    await lambda.invoke(params).promise();
-    console.log(`{func} lambda warm up!`);
+    
+    const invokeResult = await lambda.invoke(params).promise();
+    console.log(`${functionName} concurrency ${isNaN(func.concurrency)?1:func.concurrency} lambda warm up!`);
+    console.log("invokeResult", invokeResult);
 
     return true;
   }));
