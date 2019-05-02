fa6e5b35cbce9c42e0f6ee1dd2030beded57c881
jay <ymggt@naver.com>
2019-05-02 16:44:56 +0900

    [P1] QA4 이슈 #5 (Jay)

--- lambda-warmup/src/warmup.js
+++ lambda-warmup/src/warmup.js
@@ -5,12 +5,13 @@ const lambda = new aws.Lambda({region: region});
 
 module.exports.handler = async (event, context, callback) => {
 
-  
+  console.log("Warm Up Start");
+
   const {prefix, functions} = warmupConfig;
 
   const payload = JSON.stringify({ source: 'lambda-warmup'});
   
-  const result = await Promise.all(functions.map(async (func) => {
+  const invokes = await Promise.all(functions.map(async (func) => {
     const functionName = prefix?prefix.concat(func.name):func.name;
     const params = {
       ClientContext: Buffer.from(payload).toString('base64'),
@@ -20,13 +21,21 @@ module.exports.handler = async (event, context, callback) => {
       Qualifier: "$LATEST",
       Payload: payload
     };
-    
-    const invokeResult = await lambda.invoke(params).promise();
-    console.log(`${functionName} concurrency ${isNaN(func.concurrency)?1:func.concurrency} lambda warm up!`);
-    console.log("invokeResult", invokeResult);
 
-    return true;
+    try {
+      await Promise.all(Array(func.concurrency).fill(0)
+        .map(async _ => await lambda.invoke(params).promise()));
+
+      console.log(`${functionName} concurrency ${isNaN(func.concurrency)?1:func.concurrency} lambda warm up!`);
+      return true;
+    } catch (e) {
+      console.log(`Warm Up Invoke Error: ${func.name}`, e);
+      return false;
+    }
+   
   }));
+  
+  console.log(`Warm Up Finished with ${invokes.filter(r => !r).length} invoke errors`);
 
   return callback(null, "warm up lambda!");
 };
