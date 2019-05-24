2707c95607ed30140d657abe001364fe0e036431
jay <ymggt@naver.com>
2019-05-24 12:39:42 +0900

    update

--- restapis/controllers/tag/list.js
+++ restapis/controllers/tag/list.js
@@ -1,13 +1,13 @@
 'use strict';
 const documentService = require('../document/documentMongoDB');
 module.exports.handler = async (event, context, callback) => {
-
+  console.log(event);
   /** Immediate response for WarmUp plugin */
   if (event.source === 'lambda-warmup') {
     console.log('WarmUp - Lambda is warm!')
     return callback(null, 'Lambda is warm!')
   }
-
+  
   const {t} = event.query;
   
   const resultList = await documentService.getTopTag(t);
