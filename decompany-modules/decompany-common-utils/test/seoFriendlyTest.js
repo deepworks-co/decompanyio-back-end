0153597b5d96cb79c76295c000bc8024ebcce38b
jay <ymggt@naver.com>
2019-05-24 11:48:36 +0900

    unit test code

---
+++ decompany-modules/decompany-common-utils/test/seoFriendlyTest.js
@@ -0,0 +1,6 @@
+const {utils} = require("../index.js");
+
+test=(str)=>console.log(">", str);
+
+test(utils.toSeoFriendly("맥북 프로로 개발 잘하는법 준비 됐나?"));
+test(utils.toSeoFriendly("macbook pro로 개발 잘하는법 r u ready?"));
