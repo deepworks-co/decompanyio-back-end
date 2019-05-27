ccc8fee399500e229eab060daff6e1bcf69f70f9
jay <ymggt@naver.com>
2019-05-27 18:05:06 +0900

    [P1] AboutUs 페이지를 통한 사전 가입 이메일 수집 기능 구현

---
+++ verify-email/src/email/verifyEmail.js
@@ -0,0 +1,75 @@
+'use strict';
+const crypto = require('crypto')
+const { mongodb, tables, sesConfig, applicationConfig} = require('decompany-app-properties');
+const { utils, MongoWapper} = require('decompany-common-utils');
+
+const TB_VERIFY_EMAIL = tables.VERIFY_EMAIL;
+
+module.exports.handler = async (event, context, callback) => {
+
+  console.log(event);
+  const { code } = event.path;
+  const referer = event.headers?event.headers.Referer:undefined;
+
+  if(!code){
+    throw new Error("parameter is invalid!!!");
+  }
+
+  const verifyRequest = await getVerifyRequest(code);
+
+  if(!verifyRequest){
+    throw new Error(`No Verify Request  : ${code}`)
+  }
+
+  if(verifyRequest.verify){
+    throw new Error(`already verified  : ${code}`)
+  }
+
+  const result = await verifyEmail({_id: verifyRequest._id, referer: referer});
+  console.log("verifyEmail", result);
+  
+  const response = JSON.stringify({
+    success: true
+  })
+    
+  return response;
+
+};
+
+
+async function getVerifyRequest(code){
+
+  const wapper = new MongoWapper(mongodb.endpoint);
+    
+  try{
+    const result = await wapper.find(TB_VERIFY_EMAIL, {_id: code}, {created:- 1}, 1);
+
+    return result[0];
+    
+  } catch(err){
+    throw err;
+  } finally {
+    wapper.close();
+  }
+
+}
+async function verifyEmail(params){
+  const wapper = new MongoWapper(mongodb.endpoint);
+    
+  try{
+    const result = await wapper.update(TB_VERIFY_EMAIL, {_id: params._id}, {
+      $set: {
+        verify: true,
+        verified: Date.now(),
+        verifiedReferer: params.referer
+      }
+    });
+
+    return result;
+    
+  } catch(err){
+    throw err;
+  } finally {
+    wapper.close();
+  }
+}
