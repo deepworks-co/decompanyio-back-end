c092d957ccae0e91d1307a93da260d40548c7690
jay <ymggt@naver.com>
2019-07-31 14:38:29 +0900

    s3 event  관련  함수를 converter-wapper로 이동

--- restapis/install.sh
+++ restapis/install.sh
@@ -2,7 +2,6 @@ npm install ../decompany-modules/decompany-common-utils/
 npm install ../decompany-modules/decompany-app-properties/
 npm install --save-dev serverless-mocha-plugin
 npm install --save-dev serverless-offline
-npm install --save-dev serverless-plugin-existing-s3
 npm install --save-dev serverless-aws-documentation
 npm install --save-dev serverless-plugin-split-stacks
 npm install --save-dev serverless-plugin-canary-deployments
