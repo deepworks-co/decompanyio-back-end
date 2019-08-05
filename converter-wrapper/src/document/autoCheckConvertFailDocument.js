438b17226879c8394806d1185e349f7a78addc0b
jay <ymggt@naver.com>
2019-08-05 10:04:42 +0900

    bugfix

--- converter-wrapper/src/document/autoCheckConvertFailDocument.js
+++ converter-wrapper/src/document/autoCheckConvertFailDocument.js
@@ -8,18 +8,20 @@ module.exports.handler = async (event, context, callback) => {
 
   const result = await updateConvertFailAll(wrapper);
 
-  console.log("updateConvertFailAll", result, result.length);
+  console.log("updateConvertFailAll", result);
+
+  wrapper.close();
   
   return callback(null, {success: true, result});
 };
 
 
 function updateConvertFailAll(wrapper) {
-  const expiredAt = Date.now() - 1000 * 60 * 5; //
-  const {DOCUMENT} = constants;
-  const {STATE} = DOCUMENT;
+  const failAt = Date.now() - 1000 * 60 * 5; //5mins
+  const {STATE} = constants.DOCUMENT;
+
   return new Promise((resolve, reject) => {
-    const query = {state: {$nin: [STATE.CONVERT_COMPLETE, STATE.CONVERT_FAIL]}, created: {$lt: expiredAt}}
+    const query = {state: {$in: [STATE.UPLOAD_COMPLETE, STATE.NOT_CONVERT]}, created: {$gt: failAt}}
     wrapper.update(tables.DOCUMENT, query, {$set: {state: STATE.CONVERT_FAIL}}, {multi: true})
     .then((data)=>{
       resolve(data);
