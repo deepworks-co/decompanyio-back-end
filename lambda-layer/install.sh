93e3b7e11de29815decb541ad4c369d976e07b2c
jay <ymggt@naver.com>
2019-09-19 14:05:29 +0900

    update

--- lambda-layer/install.sh
+++ lambda-layer/install.sh
@@ -3,3 +3,5 @@ npm install --save-dev serverless-mocha-plugin
 npm install --save-dev serverless-prune-plugin
 npm install sharp@0.22.1
 npm install geoip-lite
+npm install --prefix sharp sharp@0.22.1
+npm install --prefix geoip geoip-lite
