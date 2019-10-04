cc2fb368a074b62684467a89595f14bcafca2b52
jay <ymggt@naver.com>
2019-10-04 11:01:33 +0900

    convertKeysToLowerCase 추가
      - json의 모든 키를 lowercase로 변경한다.

--- decompany-modules/decompany-common-utils/src/utils.js
+++ decompany-modules/decompany-common-utils/src/utils.js
@@ -81,3 +81,18 @@ exports.parseBool = (v, defaultValue) =>{
 
   return false;
 }
+
+exports.convertKeysToLowerCase = (obj) => {
+  const output = {};
+  for (i in obj) {
+      if (Object.prototype.toString.apply(obj[i]) === '[object Object]') {
+         output[i.toLowerCase()] = ConvertKeysToLowerCase(obj[i]);
+      }else if(Object.prototype.toString.apply(obj[i]) === '[object Array]'){
+          output[i.toLowerCase()]=[];
+           output[i.toLowerCase()].push(ConvertKeysToLowerCase(obj[i][0]));
+      } else {
+          output[i.toLowerCase()] = obj[i];
+      }
+  }
+  return output;
+};
