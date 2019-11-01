9d44270c065446a37b5974d790c1c600c183dfcb
jay <ymggt@naver.com>
2019-11-01 14:43:57 +0900

    add sns error publish

---
+++ decompany-modules/decompany-common-utils/src/sns.js
@@ -0,0 +1,14 @@
+const AWS = require("aws-sdk");
+const sns = new AWS.SNS();
+/**
+ * @param regions : 
+ * @param querueUrl : 
+ * @param messageBody
+ */
+exports.errorPublish = (topic, params) => {
+    const sns = new AWS.SQS({region: region});
+    return sns.publish({
+      Message: JSON.stringify(params),
+      TopicArn: topic
+    })
+ }
