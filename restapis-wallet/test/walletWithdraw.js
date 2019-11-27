f658c27988a1bd8d73deed678135a7814e3ea209
jay <ymggt@naver.com>
2019-11-27 11:04:54 +0900

    update

--- restapis-wallet/test/walletWithdraw.js
+++ restapis-wallet/test/walletWithdraw.js
@@ -16,8 +16,7 @@ describe('walletWithdraw', () => {
     const event = {
       principalId: "google-oauth2|101778494068951192848",
       body: {
-        amount: 1,
-        toAddress: "0xa05b51311397C5552798Ce216250BC0e757c1Aa2"
+        amount: 1
       }
     }
     return wrapped.run(event).then((response) => {
