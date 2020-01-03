f983be45d5e32f70134a814984bd9aa21993147e
jay <ymggt@naver.com>
2020-01-03 18:13:00 +0900

    Update royalty 보상 저장시 db schema 개선

--- restapis-wallet/src/claim/royalty.js
+++ restapis-wallet/src/claim/royalty.js
@@ -1,24 +1,13 @@
 'use strict';
-const {stage, mongodb, tables, region, walletConfig} = require("decompany-app-properties");
+const {stage, mongodb, tables, region, walletConfig, applicationConfig} = require("decompany-app-properties");
 const {kms, sns, MongoWrapper, utils} = require("decompany-common-utils");
 const Web3 = require('web3');
-const Transaction = require('ethereumjs-tx');
-const {buildContract} = require('../ContractUtils');
 
 const web3 = new Web3(walletConfig.psnet.providerUrl);
 const mongo = new MongoWrapper(mongodb.endpoint);
+const ACTIVE_VOTE_DAYS = applicationConfig.activeRewardVoteDays;
 
-/*
-const PSNET_BALLOT_ABI = require(`../../psnet/${stage}/Ballot.json`)
-const PSNET_DECK_ABI = require(`../../psnet/${stage}/ERC20.json`)
-const PSNET_REGISTRY_ABI = require(`../../psnet/${stage}/Registry.json`)
-const NETWORK_ID = walletConfig.psnet.id;
-
-const DECK_CONTRACT = buildContract(web3, PSNET_DECK_ABI, NETWORK_ID);
-const BALLOT_CONTRACT = buildContract(web3, PSNET_BALLOT_ABI, NETWORK_ID);
-const REGISTRY_CONTRACT = buildContract(web3, PSNET_REGISTRY_ABI, NETWORK_ID);
-*/
-module.exports.handler = async (event, context, callback) => {
+module.exports.handler = async (event) => {
   //context.callbackWaitsForEmptyEventLoop = false;
   if (event.source === 'lambda-warmup') {
     console.log('WarmUp - Lambda is warm!')
@@ -47,62 +36,32 @@ module.exports.handler = async (event, context, callback) => {
       documentId: documentId,
       userId: principalId
     });
-    console.log("lastClaim", lastClaim);
+  
     const start = lastClaim&&lastClaim.created?new Date(lastClaim.created + (1000 * 60 * 60 * 24)):new Date(0); //마지막 claim에서 다음날부터 claim요청함
     const end = new Date();
-    const pageviews = await getPageview(documentId, utils.getBlockchainTimestamp(start), utils.getBlockchainTimestamp(end));
-    
-    const pageviewWithRewardPromises = pageviews.map(async (pageviewInfo)=>{
+    const pageviews = await getPageviewList(documentId, utils.getBlockchainTimestamp(start), utils.getBlockchainTimestamp(utils.getDate(end, ACTIVE_VOTE_DAYS * -1)));
+    //console.log("pageviews", JSON.stringify(pageviews));
 
-      const reward = await calcReward({
-        year: pageviewInfo._id.year,
-        month: pageviewInfo._id.month,
-        dayOfMonth: pageviewInfo._id.dayOfMonth,
-        blockchainTimestamp: pageviewInfo.blockchainTimestamp,
-        principalId, 
-        documentId,
-        pageview: pageviewInfo.pageview,
-        totalPageview: pageviewInfo.totalpageviewInfo.totalPageview
-      });
-
-      return {
-        _id: {
-          year: pageviewInfo._id.year, 
-          month: pageviewInfo._id.month, 
-          dayOfMonth: pageviewInfo._id.dayOfMonth, 
-          userId: principalId, 
-          documentId: documentId
-        }, 
-        blockchainTimestamp: pageviewInfo.blockchainTimestamp, 
-        value: reward
-      }
+    const calcRoyaltiesResult = await calcRoyalties({
+      documentId, 
+      userId: principalId, 
+      pageviews
     })
-    const pageviewWithReward = await Promise.all(pageviewWithRewardPromises);
-    console.log("royalties", pageviewWithReward);
-    
-    //save
-    const savePromises = pageviewWithReward.filter((it)=>{
-      if(it.value)
-        return true;
-      else
-        return false;
-    }).map(async (it)=>{
+    console.log("calcRoyaltiesResult", JSON.stringify(calcRoyaltiesResult));
 
-      await saveClaimReward(it);
-      await saveWallet({royaltyId: it._id, value: it.value});
-
-      return it;
-    })
-    const savePromisesComplete = await Promise.all(savePromises);
-
-    console.log("save royalty claim", savePromisesComplete);
-
+    const saveRoyaltiesResult = await saveRoyalties({
+      documentId, 
+      userId: principalId, 
+      pageviews,
+      royalties: calcRoyaltiesResult
+    });
+  
+    //console.log("saveRoyaltiesResult", JSON.stringify(saveRoyaltiesResult));
     return JSON.stringify({
       success: true,
-      royalties: pageviewWithReward.map((it)=>{
+      royalties: saveRoyaltiesResult.map((it)=>{
         return {
           _id: it._id,
-          blockchainTimestamp: it.blockchainTimestamp,
           value: it.value.toString()
         }
       })
@@ -114,29 +73,53 @@ module.exports.handler = async (event, context, callback) => {
 
 };
 
-async function calcReward(args){
+/**
+ * 
+ * @param {*} param0 
+ */
+async function calcRoyalties({pageviews, userId, documentId}) {
 
-  const {totalPageview, pageview, year, month, dayOfMonth, blockchainTimestamp} = args;
-  const date = new Date(Date.UTC(year, month-1, dayOfMonth));
-  const rewardPool = await getRewardPool(date);
+  const pageviewWithRewardPromises = pageviews.map(async (pageviewInfo)=>{
+    const {pageview, blockchainTimestamp, totalpageviewInfo, rewardPoolInfo} = pageviewInfo;
+    const reward = await calcRoyalty({
+      blockchainTimestamp: blockchainTimestamp,
+      pageview: pageview,
+      totalPageview: totalpageviewInfo?totalpageviewInfo.totalPageview:0,
+      creatorDailyReward: rewardPoolInfo?rewardPoolInfo.creatorDailyReward:0
+    });
+
+    return {
+      _id: {
+        documentId: documentId,
+        userId: userId, 
+        blockchainTimestamp: pageviewInfo.blockchainTimestamp
+      },
+      value: reward
+    }
+  })
+  const pageviewWithReward = await Promise.all(pageviewWithRewardPromises);
+  
+  return pageviewWithReward;
+}
+
+/**
+ * 
+ * @param {*} param0 
+ */
+async function calcRoyalty({totalPageview, pageview, creatorDailyReward}){
   let reward = 0;
-  
-  if(rewardPool){
-    //console.log("calcReward : ", JSON.stringify(_id), stat.pageview, totalpageview.totalPageview, rewardPool.creatorDailyReward);
-
-    let royalty = (pageview / totalPageview)  * rewardPool.creatorDailyReward;
-    royalty  = Math.floor(royalty * 100000) / 100000;
-    const strValue = web3.utils.toWei(royalty + "", "ether");
-
-    reward = MongoWrapper.Decimal128.fromString(strValue);
-  } else {
-    //console.log("calcReward : 0");
-    reward = MongoWrapper.Decimal128.fromString("0");
+  if(creatorDailyReward){
+    reward = utils.calcRoyalty({totalPageview, pageview, creatorDailyReward});
   }
-  
+   
   return reward;
 }
 
+
+/**
+ * 
+ * @param {*} documentId 
+ */
 function getDocument(documentId){
   return new Promise((resolve, reject)=>{
     mongo.findOne(tables.DOCUMENT, {_id: documentId}).then((data)=>{
@@ -147,7 +130,13 @@ function getDocument(documentId){
   })
 }
 
-function getPageview(documentId, startTimestamp, endTimestamp){
+/**
+ * 
+ * @param {*} documentId 
+ * @param {*} startTimestamp 
+ * @param {*} endTimestamp 
+ */
+function getPageviewList(documentId, startTimestamp, endTimestamp){
   return new Promise((resolve, reject)=>{
     mongo.aggregate(tables.STAT_PAGEVIEW_DAILY, [{
       $match: {
@@ -165,8 +154,20 @@ function getPageview(documentId, startTimestamp, endTimestamp){
         as: 'totalpageviewInfo'
       }
     }, {
+      $lookup: {
+        from: tables.REWARD_POOL_DAILY,
+        localField: 'blockchainTimestamp',
+        foreignField: '_id',
+        as: 'rewardPoolInfo'
+      }
+    }, {
       $unwind: {
         path: "$totalpageviewInfo",
+        preserveNullAndEmptyArrays: true
+      }
+    }, {
+      $unwind: {
+        path: "$rewardPoolInfo",
         preserveNullAndEmptyArrays: true
       }
     }, {
@@ -178,45 +179,6 @@ function getPageview(documentId, startTimestamp, endTimestamp){
     })
   })
 }
-
-
-/**
- * 
- * @param {*} params 
- */
-function saveClaimReward(params){
-  return new Promise(async (resolve, reject)=>{
-    mongo.save(tables.CLAIM_ROYALTY, {
-      _id: params._id,
-      blockchainTimestamp: params.blockchainTimestamp,
-      value: params.value,
-      created: Date.now()
-    }).then((data)=>{
-      resolve(data);
-    }).catch((err)=>{
-      reject(err);
-    });
-  });
-}
-
-function saveWallet({royaltyId, value}){
-
-  return new Promise(async (resolve, reject)=>{
-    mongo.save(tables.WALLET, {
-      userId: royaltyId.userId,
-      type: "ROYALTY",
-      factor: 1,
-      value ,
-      royaltyId: royaltyId,
-      created: Date.now()
-    }).then((data)=>{
-      resolve(data);
-    }).catch((err)=>{
-      reject(err);
-    });
-  });
-}
-
 
 /**
  * @name getLastClaimRoyalty
@@ -246,22 +208,33 @@ function getLastClaimRoyalty(params){
   });
 }
 
-function getRewardPool(date){
- 
-  return new Promise((resolve, reject)=>{
-    mongo.find(tables.REWARD_POOL, {
-      query: {
-        "_id.start": {$lte: date}, "_id.end": {$gt: date}
-      }
-    }).then((data)=>{
-      if(data && data.length>0){
-        resolve(data[0]);
-      } else {
-        resolve(null);
-      }
-      
-    }).catch((err)=>{
-      reject(err);
-    })
+/**
+ * 
+ * @param {*} param0 
+ */
+async function saveRoyalties({documentId, userId, pageviews, royalties}){
+  //save
+  const savePromises = royalties.map(async (it)=>{
+
+    const saveClaimRoyalty = await mongo.save(tables.CLAIM_ROYALTY, {
+      _id: it._id,
+      value: MongoWrapper.Decimal128.fromString(it.value + ""),
+      created: Date.now()
+    });
+
+
+    const saveWalletResult = await mongo.save(tables.WALLET, {
+      userId: userId,
+      type: "ROYALTY",
+      factor: 1,
+      value: MongoWrapper.Decimal128.fromString(web3.utils.toWei(it.value + "", "ether")),
+      claimRoyalty: saveClaimRoyalty,
+      created: Date.now()
+    });
+
+    return it;
   })
+  const saveRoyaltiesResult = await Promise.all(savePromises);
+  
+  return saveRoyaltiesResult;
 }
