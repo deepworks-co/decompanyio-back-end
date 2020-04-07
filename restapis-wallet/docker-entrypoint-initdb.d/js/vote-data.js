a73afdd3fd7e9c49c5102675d7ef07d46d795ac6
jay <jay@polarishare.com>
2020-04-07 10:31:36 +0900

    update test data

--- restapis-wallet/docker-entrypoint-initdb.d/js/vote-data.js
+++ restapis-wallet/docker-entrypoint-initdb.d/js/vote-data.js
@@ -8,11 +8,12 @@ function getDate(date, days) {
   return new Date(baseDate.setDate(baseDate.getDate() + days));
 }
 
-function saveCuratorRewardData(baseDate, pageview) {
+function saveCuratorRewardData({documentId, userId, baseDate, pageview, voteAmount}) {
+  print('saveCuratorRewardData', {documentId, userId, baseDate, pageview, voteAmount})
   const blockchainTimestamp = getBlockchainTimestamp(baseDate);
   const blockchainDate = new Date(blockchainTimestamp)
 
-  db.VOTE.save({documentId, userId, "deposit": NumberDecimal("4300000000000000000"),"blockchainTimestamp": getBlockchainTimestamp(nowDate),"created": nowDate.getTime()})
+  db.VOTE.save({documentId, userId, "deposit": NumberDecimal(voteAmount),"blockchainTimestamp": getBlockchainTimestamp(baseDate),"created": baseDate.getTime()})
   
   db["STAT-PAGEVIEW-DAILY"].save({_id: {
     year: baseDate.getUTCFullYear(), month: baseDate.getUTCMonth() + 1, dayOfMonth: baseDate.getUTCDate(), id: documentId
@@ -20,17 +21,30 @@ function saveCuratorRewardData(baseDate, pageview) {
 
   const totalPageview = pageview
   const totalPageviewSquare = Math.pow(pageview, 2)
-  const count = 1
+  const count = 1 //한개의 문서(의미 없음, 기록용)
   db["STAT-PAGEVIEW-TOTALCOUNT-DAILY"].save({_id: {
     year: baseDate.getUTCFullYear(), month: baseDate.getUTCMonth() + 1, dayOfMonth: baseDate.getUTCDate()
   }, totalPageview, totalPageviewSquare, count, blockchainDate, blockchainTimestamp, created: baseDate.getTime()})
 }
+
+/**
+ * 20일간 하나의 문서에 대해서 4.3 vote, 1 pageview의 테스트 데이터 생성
+ */
 const userId = "google-oauth2|101778494068951192848"
 const documentId = "feed7f026db54859bec3221dcad47d8f";
 const BEFORE_DAYS = 20;
-let nowDate = getDate(new Date(), (BEFORE_DAYS * -1));
+const startDate = getDate(new Date(), (BEFORE_DAYS * -1));
 
+// data input
 for(let i=0;i<BEFORE_DAYS;i++){
-  nowDate = getDate(nowDate, 1);
-  saveCuratorRewardData(nowDate, 1);
+  const nowDate = getDate(startDate, i);
+
+  saveCuratorRewardData({
+    documentId: documentId, 
+    userId: userId,
+    baseDate: nowDate,
+    pageview: 1,
+    voteAmount: "4000000000000000000"
+  }); //4 deck
+
 }
